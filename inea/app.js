3e90e2ee2dd9c4d0a2acb21548759f451da779bc
Wil <ca.wilfrido@gmail.com>
2021-07-19 22:32:38 -0500

    filter for visualization

--- inea/app.js
+++ inea/app.js
@@ -1,6 +1,14 @@
 const graf = d3.select('#graf')
 const anchoTotal = graf.style('width').slice(0, -2)
 const altoTotal = (anchoTotal * 9) / 16
+/*
+var indice = document.name_select.selectedIndex 
+var valor = document.name_select.options[indice].value 
+*/
+var ob = document.getElementById("id_sel");
+var indiceee= ob.selectedIndex;
+var valor = ob.options[indiceee].value;
+var tmp_barras = null;
 
 const svg = graf
   .append('svg')
@@ -23,7 +31,10 @@ const g = svg
   .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 let allData = []
-
+let allData2 = []
+let fiveStates = []
+let five_temp ;
+function init(){
 d3.csv('DATOS_INEA_2019.csv').then(data => {
   data.forEach(d => {
    d.MetaAnual = parseInt(d.MetaAnual.split(",").join("").trim());
@@ -33,16 +44,44 @@ d3.csv('DATOS_INEA_2019.csv').then(data => {
 
 
   console.log("data en for each: " + data)
-
+  const CFX = crossfilter(data);
+  const dimension = CFX.dimension(function (d){ return d.MetaAnual; });
+  
+  console.dir(dimension.top(Infinity).length);
+  fiveStates = dimension.filterRange([15000, 30000]);
+  console.log("fiveStates: " + fiveStates);
+  console.dir("dimension.top" + dimension.top(Infinity));
+   five_temp=  dimension.top(Infinity);
+  console.log(five_temp);
+  console.dir("dimension.top(Infinity).length "+ dimension.top(Infinity).length);
   allData = data.slice(0, 32)
+  allData2 = five_temp.slice(0, 5)
+  console.log("allData: " + allData);
+  //fiveStates = data.slice(0, 5);
   console.log("todos los datos despues de aplicar slice " + allData)
 
-  render(allData)
+  
+
+  if(valor === "todos"){
+    console.log("el valor elegido es: " + valor);
+    render(allData)
+  }else if(valor == "altas15"){
+
+    if(xAxisGroup != null){
+      xAxisGroup.empty;
+    }
+    render(five_temp);
+    console.log("el valor elegido es (most5: " + valor);
+  }
 })
+}
 
 function render(data) {
   // [binding] ENTER - update - exit
-  let barras = g.selectAll('rect').data(data)
+  let barras = g.selectAll('rect')
+  .remove()
+  .exit()
+  .data(data)
 
 
   let y = d3.scaleLinear()
@@ -57,11 +96,10 @@ function render(data) {
 
   let color = d3.scaleOrdinal()
     .domain(d3.map(allData, d => d.region))
-    // .range(d3.schemeCategory10)
     .range(['#7CB9E8'])
 
 
-  const xAxisGroup = g.append('g')
+   xAxisGroup = g.append('g')
     .attr('transform', `translate(0, ${alto})`)
     .attr('class', 'ejes')
     .call(
@@ -73,14 +111,16 @@ function render(data) {
     .attr('transform', 'rotate(-90)')
     .attr('y', -15)
     .attr('x', -10)
-
+console.log(xAxisGroup);
   const yAxisGroup = g.append('g')
       .attr('class', 'ejes')
       .call(
         d3.axisLeft(y)
           .ticks(4)
           .tickSize(-ancho)
-          .tickFormat(d => `${d} m.`)
+          .tickFormat(d => `${d}`)
+          //.tickFormat(d3.format("s"))
+          
       )
 
   barras
@@ -97,7 +137,7 @@ function render(data) {
         .attr('y', d => y(d.MetaAnual))
         .attr('fill', d => color(d.region))
         .attr('height', d => alto - y(d.MetaAnual))
-
+        
   titleGroup = g.append('g')
         .append('text')
         .attr('text-anchor', 'middle')
@@ -105,4 +145,16 @@ function render(data) {
         .attr('y', -10)
         .attr('class', 'titulo')
         .text('Meta anual de alfabetización INEA 2019')
+
+}
+
+init();
+
+
+function changeFunc(){
+ob = document.getElementById("id_sel");
+indiceee= ob.selectedIndex;
+valor = ob.options[indiceee].value;
+svg.selectAll("g.ejes").remove();
+init();
 }
